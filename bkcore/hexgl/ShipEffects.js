776c12fba0eeae4a90b16bd101e784c3f908f32c
01-menjivar <00068422@uca.edu.sv>
2026-05-17 15:41:49 -0600

    chore: add MIT license headers and attribution to project files

--- bkcore/hexgl/ShipEffects.js
+++ bkcore/hexgl/ShipEffects.js
@@ -3,6 +3,8 @@
  * @author Thibaut 'BKcore' Despoulain <http://bkcore.com>
  * @license This work is licensed under the Creative Commons Attribution-NonCommercial 3.0 Unported License.
  *          To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/3.0/.
+ *
+ * Extended by Equipo Pepsiman under MIT License
  */
 
 var bkcore = bkcore || {};
